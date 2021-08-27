a6c2180f56c4e38cfe2b686bf30306c1e6c8ea35
becca4011 <becca4011@naver.com>
2021-08-27 17:47:18 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
+
+// props.fav === {fav} (props 안의 값인 fav를 가져오는 방법)
+
+// {array.map(function => (<Component name = {array.name} />))}
+// array 안의 name을 하나씩 가져와서, Component의 인자에 들어감
+
+// Component.propTypes = {name: PropTypes.[type].isRequired};
+// PropTypes로 string, number, array, boolean, object 등을 체크할 수 있음
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
