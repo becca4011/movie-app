83e028f0bd7094059d0e170d38abab9302c4100d
becca4011 <becca4011@naver.com>
2021-08-28 18:58:25 +0900

    state

--- src/App.js
+++ src/App.js
@@ -9,12 +9,32 @@ import PropTypes from "prop-types";
 // Component.propTypes = {name: PropTypes.[type].isRequired};
 // PropTypes로 string, number, array, boolean, object 등을 체크할 수 있음
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    // 바꿀 수 있는 data
+    count: 0
+  };
 
-    </div>
-  );
+  // JavaScript 코드
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+    // current : 현재 state
+  };
+
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+
+  // setState()를 사용하면 render() 호출
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
