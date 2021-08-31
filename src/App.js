74188358e4073dff98bf7846240d88e60d38707b
becca4011 <becca4011@naver.com>
2021-08-31 18:30:04 +0900

    movie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 // props.fav === {fav} (props 안의 값인 fav를 가져오는 방법)
 
@@ -14,18 +16,33 @@ class App extends React.Component {
     movies: []
   };
 
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    // axios가 끝날 때 까지 기다림
+    // async를 사용해야 await를 사용 가능
+    this.setState({ movies, isLoading: false })
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000); // 6초 후에 isLoading을 false로 바꿈
+    this.getMovies();
   }
 
   // setState()를 사용하면 render() 호출
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        {isLoading ? "Loading..." : "We are ready"}
+        {/* 삼항연산자 */}
+        {isLoading ? "Loading..." : movies.map(movie => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.id}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+          />
+        ))}
       </div>
     );
   }
